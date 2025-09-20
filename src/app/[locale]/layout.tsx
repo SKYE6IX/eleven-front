8cd7bdfe0646a5eb2e36abbe665e2de7d7507028
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2025-09-20 13:16:23 +0300

    Added jsonLD file and improve SEO

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,16 +2,17 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
-import { setRequestLocale } from "next-intl/server";
+import {
+   setRequestLocale,
+   getTranslations,
+   getMessages,
+} from "next-intl/server";
 import { routing } from "@/i18n/routing";
 import Navigation from "@/components/navigation/Navigation";
 import GsapWrapper from "@/context/GsapWrapper";
+import { getJsonLd } from "@/utils/getJsonLd";
 import "../../global-styles/globals.scss";
 
-export const metadata: Metadata = {
-   title: "Eleven Web Agency in Moscow",
-   description: "We are web design agency located in Moscow.",
-};
 const fontMontreal = localFont({
    src: [
       {
@@ -48,6 +49,68 @@ const fontMontrealItalic = localFont({
    variable: "--font-montreal-italic",
 });
 
+export async function generateMetadata({
+   params,
+}: {
+   params: { locale: string };
+}): Promise<Metadata> {
+   const { locale } = await params;
+   const t = await getTranslations({ locale, namespace: "SEO" });
+   return {
+      title: `${t("metaData.title")}`,
+      description: t("metaData.description"),
+      alternates: {
+         // TODO: Later
+         // canonical: "https://ventimax.ru/ru",
+         // languages: {
+         //    en: "https://ventimax.ru/en",
+         //    zh: "https://ventimax.ru/zh",
+         // },
+      },
+      robots: {
+         index: true,
+         follow: true,
+         nocache: true,
+         googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: false,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+         },
+      },
+      openGraph: {
+         title: `${t("metaData.title")}`,
+         description: t("metaData.description"),
+         url: "",
+         siteName: "Eleven",
+         images: [
+            {
+               url: "/og_image.webp",
+               width: 1200,
+               height: 630,
+               alt: "Eleven Web Development Agency Portfolio",
+            },
+         ],
+         type: "website",
+         locale: locale,
+      },
+      twitter: {
+         title: `${t("metaData.title")}`,
+         description: t("metaData.description"),
+         images: [
+            {
+               url: "/og_image.webp",
+               width: 1200,
+               height: 630,
+               alt: "Eleven Web agency portfolio",
+            },
+         ],
+      },
+   };
+}
+
 export function generateStaticParams() {
    return routing.locales.map((locale) => ({ locale }));
 }
@@ -63,6 +126,8 @@ export default async function RootLayout({
       notFound();
    }
    setRequestLocale(locale);
+   const message = await getMessages({ locale: locale });
+   const jsonLd = getJsonLd(message);
    return (
       <html
          lang={locale}
@@ -75,6 +140,12 @@ export default async function RootLayout({
                   {children}
                </GsapWrapper>
             </NextIntlClientProvider>
+            <script
+               type="application/ld+json"
+               dangerouslySetInnerHTML={{
+                  __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+               }}
+            />
          </body>
       </html>
    );
---
+++ src/utils/getJsonLd.ts
@@ -0,0 +1,173 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getJsonLd(message: Record<string, any>) {
+   const jsonLd = {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      name: message.SEO.jsonLd.name,
+      url: "",
+      logo: "/logo.png",
+      description: message.SEO.jsonLd.description,
+      founder: {
+         "@type": "Person",
+         name: "Busari Azeez Abiola",
+      },
+      foundingDate: "2025",
+      address: {
+         "@type": "PostalAddress",
+         addressLocality: "Moscow",
+         addressCountry: "Russia",
+      },
+      contactPoint: [
+         {
+            "@type": "ContactPoint",
+            telephone: "+7-993-270-7338",
+            contactType: "customer service",
+            email: "hello.madebyeleven.com",
+            availableLanguage: ["English", "Russian"],
+         },
+      ],
+      sameAs: [
+         "https://www.linkedin.com/company/pixelforge",
+         "https://www.facebook.com/pixelforge",
+         "https://twitter.com/pixelforge",
+         "https://www.instagram.com/pixelforge",
+      ],
+      email: "hello.madebyeleven.com",
+      telephone: "+7-993-270-7338",
+      areaServed: [
+         {
+            "@type": "Global",
+            name: "Word Wide",
+         },
+         {
+            "@type": "Country",
+            name: "Russia",
+         },
+      ],
+      makesOffer: [
+         {
+            "@type": "Service",
+            serviceType: message.SEO.jsonLd.services.webDesign.serviceType,
+            name: message.SEO.jsonLd.services.webDesign.name,
+            description: message.SEO.jsonLd.services.webDesign.description,
+            provider: {
+               "@type": "Organization",
+               name: message.SEO.jsonLd.name,
+            },
+            areaServed: "Global",
+            hasOfferCatalog: {
+               "@type": "OfferCatalog",
+               name: "Design Packages",
+               itemListElement: [
+                  {
+                     "@type": "Offer",
+                     position: 1,
+                     item: {
+                        "@type": "Service",
+                        name: "Responsive Design",
+                     },
+                  },
+                  {
+                     "@type": "Offer",
+                     position: 2,
+                     item: {
+                        "@type": "Service",
+                        name: "Branding Integration",
+                     },
+                  },
+               ],
+            },
+         },
+         {
+            "@type": "Service",
+            serviceType: message.SEO.jsonLd.services.webDevelopment.serviceType,
+            name: message.SEO.jsonLd.services.webDevelopment.name,
+            description: message.SEO.jsonLd.services.webDevelopment.description,
+            provider: {
+               "@type": "Organization",
+               name: message.SEO.jsonLd.name,
+            },
+            areaServed: "Global",
+            hasOfferCatalog: {
+               "@type": "OfferCatalog",
+               name: "Development Services",
+               itemListElement: [
+                  {
+                     "@type": "Offer",
+                     position: 1,
+                     item: {
+                        "@type": "Service",
+                        name: "E-commerce Development",
+                     },
+                  },
+                  {
+                     "@type": "Offer",
+                     position: 2,
+                     item: {
+                        "@type": "Service",
+                        name: "API Integration",
+                     },
+                  },
+                  {
+                     "@type": "Offer",
+                     position: 3,
+                     item: {
+                        "@type": "Service",
+                        name: "CMS Customization",
+                     },
+                  },
+               ],
+            },
+         },
+         {
+            "@type": "Service",
+            serviceType:
+               message.SEO.jsonLd.services.seoOptimization.serviceType,
+            name: message.SEO.jsonLd.services.seoOptimization.name,
+            description:
+               message.SEO.jsonLd.services.seoOptimization.description,
+            provider: {
+               "@type": "Organization",
+               name: message.SEO.jsonLd.name,
+            },
+            areaServed: "Global",
+            hasOfferCatalog: {
+               "@type": "OfferCatalog",
+               name: "SEO Packages",
+               itemListElement: [
+                  {
+                     "@type": "Offer",
+                     position: 1,
+                     item: {
+                        "@type": "Service",
+                        name: "Keyword Research and On-Page SEO",
+                     },
+                  },
+                  {
+                     "@type": "Offer",
+                     position: 2,
+                     item: {
+                        "@type": "Service",
+                        name: "Technical SEO Audit",
+                     },
+                  },
+               ],
+            },
+         },
+         {
+            "@type": "Service",
+            serviceType:
+               message.SEO.jsonLd.services.websiteMaintainace.serviceType,
+            name: message.SEO.jsonLd.services.websiteMaintainace.name,
+            description:
+               message.SEO.jsonLd.services.websiteMaintainace.description,
+            provider: {
+               "@type": "Organization",
+               name: message.SEO.jsonLd.name,
+            },
+            areaServed: "Global",
+         },
+      ],
+   };
+   return jsonLd;
+}
